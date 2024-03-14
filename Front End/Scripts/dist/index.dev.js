4211dad2099f84db7a571cbf00681fc452df9123
Bruno Sousa Trabalho <brunosousagithub@gmail.com>
2024-03-14 12:31:25 -0300

    adicionando a tela de login

---
+++ Front End/Scripts/dist/index.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var sgn = document.querySelector("#sgn");
+sgn.addEventListener("click", function () {
+  //pega os dados inseridos nos inputs na tela de login
+  var login = document.querySelector("#loginName");
+  var password = document.querySelector("#loginPassword"); // verificar se o usuario esta cadastrado no banco de dados
+});
+//# sourceMappingURL=index.dev.js.map
---
+++ Front End/Scripts/index.js
@@ -0,0 +1,11 @@
+const sgn = document.querySelector("#sgn");
+
+sgn.addEventListener("click", ()=>{
+
+    //pega os dados inseridos nos inputs na tela de login
+    const login = document.querySelector("#loginName");
+    const password = document.querySelector("#loginPassword");
+
+    // verificar se o usuario esta cadastrado no banco de dados
+
+})
